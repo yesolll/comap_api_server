caf6ac99d6bf55f6f52df7b43ba8f49b0a33ba79
yesolll <9595kys@gmail.com>
2024-08-10 13:18:40 +0900

    initial commit

---
+++ src/main/java/com/yesolll/comap_api_server/ComapApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.yesolll.comap_api_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComapApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComapApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yesolll/comap_api_server/ComapApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yesolll.comap_api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComapApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
